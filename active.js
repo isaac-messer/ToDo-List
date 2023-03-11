9704114eba2ecbc75d2cf1f962be07f577d9ac21
Isaac Messer <isaacmesser@Philip.local>
2023-03-10 17:03:06 -0700

    Finished building Add list Function

---
+++ active.js
@@ -0,0 +1,50 @@
+const listsContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
+
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY) || []);
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('Form Submitted')
+    const listName = newListInput.value;
+    if (listName == null || listName === '') return;
+    const list = createList(listName);
+    newListInput.value = null;
+    lists.push(list);
+    saveAndRender();
+})
+
+function createList(name) {
+    return { id: Date.now().toString(), name: name, tasks: [] };
+}
+
+function saveAndRender() {
+    save();
+    render();
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+
+}
+
+function render() {
+    clearElement(listsContainer);
+    lists.forEach(list => {
+        const listElement = document.createElement('li');
+        listElement.dataset.listId = list.id;
+        listElement.classList.add('list-name');
+        listElement.innerText = list.name;
+        listsContainer.appendChild(listElement);
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+render()
